cf00271d0a8a0651adfd1a63e37607742f32c143
aleque <rukunyalx@gmail.com>
2024-04-03 16:52:37 +0300

    Added Json

---
+++ index.js
@@ -0,0 +1,20 @@
+const inputEl = document.getElementById("input");
+
+
+
+async function fetchAPI(word){
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    const result = await fetch(url).then((res) => 
+    res.json());
+
+
+}
+
+
+
+inputEl.addEventListener("keyup", (e)=>{
+    if(e.target.value && e.key === "Enter"){
+        fetchAPI(e.target.value);
+    }
+})
