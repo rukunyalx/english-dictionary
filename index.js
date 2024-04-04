cfc85c710d42cc997a6d7f836313803f9326c50b
aleque <rukunyalx@gmail.com>
2024-04-04 14:26:07 +0300

    fixed error on JS

--- index.js
+++ index.js
@@ -2,27 +2,20 @@ const inputEl = document.getElementById("input");
 
 
 
-async function fetchAPI(word){
-
+async function fetchAPI(word) {
+    
     try {
-        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-
+        const url = `https://api.dictionaryapi.dev/api/
+        v2/entries/en/${word}`;
         const result = await fetch(url).then((res) => 
-        res.json());
+        res.json());    
         
     } catch (error) {
-        
-    }
-    
-
-
-
-}
-
-
+        alert('Error! Please enter a valid word');       
+}}
 
 inputEl.addEventListener("keyup", (e)=>{
     if(e.target.value && e.key === "Enter"){
         fetchAPI(e.target.value);
     }
-})
+});
