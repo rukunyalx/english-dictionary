f4780437573de0523ad2a839a7a4e9f846e87373
aleque <rukunyalx@gmail.com>
2024-04-09 17:43:29 +0300

    Added functionality to the inner HTML

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const inputEl = document.getElementById("input");
-
-
+const infoTextEl = document.getElementById("info-text");
+const meaningContainerEl = document.getElementById("meaning-container");
+const titleEl = document.getElementById("title");
+const meaningEl = document.getElementById("meaning");
 
 async function fetchAPI(word) {
-    
-    try {
-        const url = `https://api.dictionaryapi.dev/api/
-        v2/entries/en/${word}`;
-        const result = await fetch(url).then((res) => 
-        res.json());    
-        
-    } catch (error) {
-        alert('Error! Please enter a valid word');       
-}}
 
-inputEl.addEventListener("keyup", (e)=>{
-    if(e.target.value && e.key === "Enter"){
-        fetchAPI(e.target.value);
-    }
+  try {
+    infoTextEl.style.display = "block";
+    meaningContainerEl.style.display = "none";
+    infoTextEl.innerText.style.display = `Searching the meaning
+    of "${word}`;
+    const url = `https://api.dictionaryapi.dev/api/
+    v2/entries/en/${word}`;
+    const result = await fetch(url).then((res) => 
+    res.json());
+
+
+    infoTextEl.style.display = "none";
+    meaningContainerEl.style.display = "block";
+    titleEl.innerText = `"${result[0].word}"`;
+  } catch (error) {
+    alert("Error! Please enter a valid word");
+  }
+}
+
+inputEl.addEventListener("keyup", (e) => {
+    if (e.target.value && e.key === "Enter") {
+    fetchAPI(e.target.value);
+  }
 });
