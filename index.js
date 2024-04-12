385f9d4103a502563020adf6b683bc9a59249ea0
aleque <rukunyalx@gmail.com>
2024-04-12 11:59:56 +0300

    Fixed the bug that was not displaying the inner html

--- index.js
+++ index.js
@@ -5,28 +5,26 @@ const titleEl = document.getElementById("title");
 const meaningEl = document.getElementById("meaning");
 
 async function fetchAPI(word) {
-
   try {
-    infoTextEl.style.display = "block";
-    meaningContainerEl.style.display = "none";
-    infoTextEl.innerText.style.display = `Searching the meaning
-    of "${word}`;
+    infoTextEl.style.display = "block"; //show info text
+    meaningContainerEl.style.display = "none"; // hide meaning container
+    infoTextEl.style.display = `Searching the meaning
+    of "${word}`; // set info text
     const url = `https://api.dictionaryapi.dev/api/
     v2/entries/en/${word}`;
-    const result = await fetch(url).then((res) => 
-    res.json());
+    const result = await fetch(url).then((res) => res.json());
+    console.log(result);
 
-
-    infoTextEl.style.display = "none";
-    meaningContainerEl.style.display = "block";
-    titleEl.innerText = `"${result[0].word}"`;
+    infoTextEl.style.display = "none"; // Hide info text
+    meaningContainerEl.style.display = "block"; // show meaning container
+    titleEl.innerText = result[0].meanings[0].definitions[0].definition; // Set title
   } catch (error) {
-    alert("Error! Please enter a valid word");
+    // alert("Error! Please enter a valid word");
   }
 }
 
 inputEl.addEventListener("keyup", (e) => {
-    if (e.target.value && e.key === "Enter") {
+  if (e.target.value && e.key === "Enter") {
     fetchAPI(e.target.value);
   }
 });
